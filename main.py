63810f366849427ed46842cd975579a37630441e
Crocussys <egor.vladislavovich.nn@gmail.com>
2023-12-21 12:34:12 +0300

    Done

--- main.py
+++ main.py
@@ -8,31 +8,33 @@ def f(x):
 
 
 def half_division(func, a, b, eps):
+    i = 0
     while b - a > 3 * eps:
         temp = (a + b) / 2
-        print(temp)
+        print(f"Итерация {i}: [{a}, {b}]")
         p = temp - eps
         q = temp + eps
         if func(p) < func(q):
-            a = q
+            b = q
         else:
             a = p
-    print((a + b) / 2)
-    return (a + b) / 2
+        i += 1
+    print(f"Ответ: {(a + b) / 2}\n")
 
 
 def golden_ratio(func, a, b, eps):
     fe = (1 + math.sqrt(5)) / 2
+    i = 0
     while abs(b - a) >= eps:
-        print((a + b) / 2)
+        print(f"Итерация {i}: [{a}, {b}]")
         x1 = b - (b - a) / fe
         x2 = a + (b - a) / fe
         if func(x1) >= func(x2):
             a = x1
         else:
             b = x2
-    print((a + b) / 2)
-    return (a + b) / 2
+        i += 1
+    print(f"Ответ: {(a + b) / 2}\n")
 
 
 def show_chart(func, x0, x1, y0, y1, delta=100):
@@ -52,9 +54,13 @@ def show_chart(func, x0, x1, y0, y1, delta=100):
 
 
 if __name__ == '__main__':
+    show_chart(f, -5, 5, -25, 25)
     xl = 1
     xr = 3
-    e = 0.1
-    show_chart(f, -10, 10, -25, 25)
-    half_division(f, xl, xr, e)
-    golden_ratio(f, xl, xr, e)
+    print(f"Выбранный отрезок: [{xl}, {xr}]\n")
+    for e in [0.1, 0.01, 0.001, 1e-06]:
+        print(f"------Epsilon = {e}------")
+        print("Метод половинного деления")
+        half_division(f, xl, xr, e)
+        print("Метод золотого сечения")
+        golden_ratio(f, xl, xr, e)
